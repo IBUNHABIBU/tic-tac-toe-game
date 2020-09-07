0ce15a47cfc5acae8afba5a4a4c82f2a0f1bd856
IBUNHABIBU <ibunhabib@gmail.com>
2020-09-07 17:49:05 +0300

    pushing the winning combination

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ function Game(){
   const computerPlayer = ComputerPlayer(board);
   let turn = 0;
   const takeTurn = () => {
+    if(board.checkForWinner()){
+      return;
+    }
     if(turn % 2 === 0) {
       humanPlayer.takeTurn();
     } else {
@@ -25,7 +28,35 @@ function Game(){
 }
 function Board(){
   const positions = Array.from(cells);
-  return { positions }
+  const checkForWinner = () => {
+    let winner = false;
+    const winningCombinations = [
+      [0,1,2],
+      [3,4,5],
+      [6,7,8],
+      [0,3,6],
+      [1,4,7],
+      [2,5,8],
+      [0,4,8],
+      [2,4,6],
+    ]
+    winningCombinations.forEach( winningCombo=>{
+      const pos0InnerText = positions[winningCombo[0]].innerText;
+      const pos1InnerText = positions[winningCombo[1]].innerText;
+      const pos2InnerText = positions[winningCombo[2]].innerText;
+      const isWinningCombo = pos0InnerText !== '' &&
+             pos0InnerText === pos1InnerText &&
+             pos1InnerText === pos2InnerText;
+      if(isWinningCombo){
+        winner = true;
+        winningCombo.forEach(index=>{
+          positions[index].classList.add('win');
+        })
+      }
+    });
+    return winner;
+  }
+  return { positions, checkForWinner };
 }
 function HumanPlayer(board){
  
