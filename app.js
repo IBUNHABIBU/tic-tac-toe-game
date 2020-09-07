2b2fe84fcacb96fbcee0c816dec9913047e27963
IBUNHABIBU <ibunhabib@gmail.com>
2020-09-07 19:57:18 +0300

    removing lint errors

--- app.js
+++ app.js
@@ -40,14 +40,14 @@ function Board(){
   const checkForWinner = () => {
     let winner = false;
     const winningCombinations = [
-      [0, 1, 2],
-      [3, 4, 5],
-      [6, 7, 8],
-      [0, 3, 6],
-      [1, 4, 7],
-      [2, 5, 8],
-      [0, 4, 8],
-      [2, 4, 6],
+      [0, 1, 2], 
+      [3, 4, 5], 
+      [6, 7, 8], 
+      [0, 3, 6], 
+      [1, 4, 7], 
+      [2, 5, 8], 
+      [0, 4, 8], 
+      [2, 4, 6], 
     ]
     winningCombinations.forEach( winningCombo=>{
       const pos0InnerText = positions[winningCombo[0]].innerText;
